eb600d18f47121d20eae9e0c4d9d19baf1cb809e
Praza <keelahuagain@gmail.com>
2024-08-04 20:01:46 +0300

    mk 1 rotation with PURE QUATERNIONS OMG

--- Scripts/gun_kiso.gd
+++ Scripts/gun_kiso.gd
@@ -6,9 +6,15 @@ extends Node3D
 
 var gun_targeting : Vector3
 
+var gun_target_quat : Quaternion
+var is_rotating : bool = false
+
+
 
 func update_targeting(new_targeting : Vector3):
-	gun_targeting = new_targeting
+	gun_targeting = (new_targeting - global_transform.origin).normalized()
+	gun_target_quat = Quaternion(Vector3.FORWARD, gun_targeting)
+	is_rotating = true
 
 
 func _process(delta: float) -> void:
@@ -16,7 +22,25 @@ func _process(delta: float) -> void:
 		align_gun_with_targeting(delta)
 
 
+
+
 func align_gun_with_targeting(delta : float):
-	var target_vector = global_position.direction_to(gun_targeting)
-	var target_basis= Basis.looking_at(target_vector)
-	basis = basis.slerp(target_basis, 0.5 * set_configuration.max_turning_speed)
+	if !is_rotating:
+		return
+	
+	var current_quat : Quaternion = quaternion
+	var interpolated_quat : Quaternion = current_quat.slerp(gun_target_quat, delta * set_configuration.max_turning_speed)
+	
+	if (interpolated_quat.angle_to(quaternion) < 0.0001):
+		is_rotating = false
+		interpolated_quat = gun_target_quat
+	quaternion = interpolated_quat
+	#
+	#
+	#var current_rotation = global_transform.basis.get_rotation_quaternion()
+	#var target_rotation = Quaternion(global_transform.basis).slerp(Quaternion(Vector3(0, 0, -1), gun_targeting), delta * 30)
+	#
+	#var step = deg_to_rad(30) * delta
+	#var new_rotation = current_rotation.slerp(target_rotation, step)
+	#global_transform.basis = Basis(new_rotation)
+	
