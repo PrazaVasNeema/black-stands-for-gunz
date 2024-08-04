1cd547f6cd8348875572e1765b72595bfdfe63fa
Praza <keelahuagain@gmail.com>
2024-08-04 21:15:07 +0300

    Gunz rotate and rotate over time with linear interpolation
    (dreaming about bouncing close to the end rotation point)

--- Scripts/gun_kiso.gd
+++ Scripts/gun_kiso.gd
@@ -9,16 +9,59 @@ var gun_targeting : Vector3
 var gun_target_quat : Quaternion
 var is_rotating : bool = false
 
+var _tween : Tween
+
+
+#func _ready() -> void:
+	
+
+@export var palka1 : Node3D
+@export var palka2 : Node3D
 
 
 func update_targeting(new_targeting : Vector3):
 	gun_targeting = (new_targeting - global_transform.origin).normalized()
+	
 	gun_target_quat = Quaternion(Vector3.FORWARD, gun_targeting)
-	is_rotating = true
+	
+	#palka2.quaternion = gun_target_quat
+	
+	var cross_product : Vector3 = 	new_targeting.cross(global_transform.origin).normalized()
+	var first_quat : Quaternion = Quaternion(cross_product, -gun_target_quat.angle_to(quaternion) * 0.90)
+	first_quat = (first_quat * quaternion).normalized()
+	
+	#palka1.quaternion = first_quat
+	
+	#is_rotating = true
+	
+	#print(first_quat)
+	
+	#print(_tween.is_running())
+	
+	#print(gun_target_quat.angle_to(quaternion))
+	
+	if (_tween):
+		_tween.stop()
+	_tween = get_tree().create_tween()
+	var required_time : float = gun_target_quat.angle_to(quaternion) / 1
+	_tween.tween_property(self, "quaternion", gun_target_quat, required_time)
+	
+	#_tween.tween_property(self, "quaternion", gun_target_quat, required_time).set_trans(Tween.TRANS_BOUNCE)
+	#_tween.tween_property(self, "quaternion", gun_target_quat, required_time)
+	
+	
+	#_tween.tween_property(self, "quaternion", first_quat, required_time)
+	#_tween.chain().tween_property(self, "quaternion", gun_target_quat, 0.2).set_trans(Tween.TRANS_BOUNCE)
+	
+	
+	#_tween.chain().tween_property(self, "quaternion", first_quat, required_time).tween_property(self, "quaternion", gun_target_quat, required_time).set_trans(Tween.TRANS_BOUNCE)
+	
+	#_tween.play()
 
 
 func _process(delta: float) -> void:
 	if gun_targeting:
+		return
 		align_gun_with_targeting(delta)
 
 
